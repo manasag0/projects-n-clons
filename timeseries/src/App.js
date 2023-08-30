e95ec45a76aa5faefe10b1e68d6718edd070c8f2
Manasa G <manasa.g7890@hotmail.com>
2023-08-30 06:55:58 +0530

    file upate

---
+++ timeseries/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+// import { Router , Route} from 'express';
+import Da ta from '../src/pages/Data'
+export default function App() {
+  return (
+    <div>
+      <Data />
+    </div>
+  )
+}
---
+++ timeseries/src/pages/Data.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Data() {
+  return (
+    <div>
+      <h1 className="text-center">Data</h1>
+      <p><a href="/data/all.json" target="_blank"><i class="fas fa-file"></i></a>&nbsp;&
+      <span style={{color: "#03658c"}}>JSON file with all the data.</span></p>
+      
+    </div>
+  )
+}
